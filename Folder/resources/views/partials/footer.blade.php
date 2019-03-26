64a6a3b2d202ef071594654c02902dc7de33d6e6
PeterJanssen <PeterJanssen@users.noreply.github.com>
2019-03-26 13:44:35 +0100

    Fix footer
    Fix footer so it is always at the bottom of the browser

--- Folder/resources/views/partials/footer.blade.php
+++ Folder/resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="panel-footer">
+<footer class="panel-footer navbar-fixed-bottom">
     <p>Made by Yusuf Destan, Peter Janssen, Ben Merken & Sander Vlayen
         @ <a class="btn-link" href="https://www.pxl.be/">Hogeschool PXL</a></p>
 </footer>
