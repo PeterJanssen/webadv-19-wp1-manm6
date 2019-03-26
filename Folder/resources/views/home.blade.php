34d3e8052875c098cfebd88e6eaa913b2c1ce065
Ben Merken <11701910@student.pxl.be>
2019-03-26 15:21:16 +0100

    Add tab title and change src for image in home.blade.php

--- Folder/resources/views/home.blade.php
+++ Folder/resources/views/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.master')
+@section('title', 'Beer Database')
 @section('content')
     <div class="row">
         <div class="col-md-12">
@@ -7,7 +8,7 @@
         </div>
         <div class="row">
             <div class="col-md-12">
-                <img class="img-responsive img-circle" src="../../public/images/beers.jpg" alt="beers">
+                <img class="img-responsive img-circle" src="{{URL::to('/images/beers.jpg')}}" alt="beers">
             </div>
         </div>
     </div>
